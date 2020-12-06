72f84663330d299cea32d1007b2fdfdbe7d2bdda
Ilia <ilia@tapingo.com>
2020-12-06 14:15:07 +0200

    add prebuilt code

---
+++ lib/typescript/elements/Defs.d.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+export default class Defs extends React.Component {
+    static displayName: string;
+    render(): JSX.Element;
+}
---
+++ lib/typescript/elements/Stop.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+declare type StopProps = {
+    parent?: React.Component;
+};
+export default class Stop extends React.Component<StopProps, {}> {
+    props: StopProps;
+    static displayName: string;
+    setNativeProps: () => void;
+    render(): null;
+}
+export {};
